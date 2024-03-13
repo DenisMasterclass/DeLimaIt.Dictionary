21581a80a28939c2d9d3ab7023a1e5b3a0cabeea
DeLimaIt <105001929+DeLimaIt@users.noreply.github.com>
2024-03-13 20:37:04 -0300

    Repository

--- src/DeLimaIt.Dictionary.Application/Features/Configuration/Repository/ConfigurationRepository.cs
+++ src/DeLimaIt.Dictionary.Application/Features/Configuration/Repository/ConfigurationRepository.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Dapper;
+using DeLimaIt.Dictionary.Application.Features.Configuration.Repository.Entities;
+using DeLimaIt.Dictionary.Application.Shared;
+using static DeLimaIt.Dictionary.Application.Features.Configuration.Repository.Sql.ConfigurationRepositorySQL;
 namespace DeLimaIt.Dictionary.Application.Features.Configuration.Repository
 {
     public class ConfigurationRepository : IConfigurationRepository
     {
+        private readonly DataContext _context;
+        public ConfigurationRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<ParameterEntity>> GetParametersAsync(ConfigurationParameterFilter filter)
+        {
+            var connection = _context.Connection;
+            var configurationModelList = await connection.QueryAsync<ParameterEntity>(GetSqlParameters,filter,_context.Transaction).ConfigureAwait(false);
+            return configurationModelList;
+        }
     }
 }
