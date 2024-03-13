efcd254fc358346a3491a6999558c88ee09e4d1f
Denis Lima <128059827+DenisMasterclass@users.noreply.github.com>
2024-03-13 00:09:44 -0300

    Add project files.

---
+++ src/DeLimaIt.Dictionary.Application/Feature/Configuration/Infra/SqlConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+
+namespace DeLimaIt.Dictionary.Application.Feature.Configuration.Infra
+{
+
+
+    public  class SqlServerConnection : ISqlConnection
+    {
+        private  readonly ILogger<SqlServerConnection> _logger;
+
+        public SqlServerConnection(ILogger<SqlServerConnection> logger)
+        {
+            _logger = logger;   
+        }
+
+        public void Connect(string conn)
+        {
+            using (IDbConnection db = new SqlConnection(conn))
+            {
+                try
+                {
+                    db.Open();
+                    Console.WriteLine("Conexão com o banco de dados SQL Server estabelecida com sucesso.");
+                    _logger.LogInformation("Conexão com o banco de dados SQL Server estabelecida com sucesso.");
+                }
+                catch (SqlException e)
+                {
+                     Console.WriteLine("Ocorreu um erro ao se conectar ao banco de dados SQL Server:");
+                    _logger.LogInformation("Ocorreu um erro ao se conectar ao banco de dados SQL Server: " + e.Message);
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ src/DeLimaIt.Dictionary.Application/Feature/ISqlConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace DeLimaIt.Dictionary.Application.Feature
+{
+    public interface ISqlConnection
+    {
+        void Connect(string conn);
+    }
+}
