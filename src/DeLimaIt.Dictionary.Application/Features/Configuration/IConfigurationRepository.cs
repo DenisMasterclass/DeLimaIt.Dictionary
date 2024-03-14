e6342fcb3523a9857f2dfc5a647ab9f3a4dc89b2
DeLimaIt <105001929+DeLimaIt@users.noreply.github.com>
2024-03-14 11:21:25 -0300

    Interface Use case

--- src/DeLimaIt.Dictionary.Application/Features/Configuration/IConfigurationRepository.cs
+++ src/DeLimaIt.Dictionary.Application/Features/Configuration/IConfigurationRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace DeLimaIt.Dictionary.Application.Features.Configuration
+﻿using DeLimaIt.Dictionary.Application.Features.Configuration.Repository.Entities;
+
+namespace DeLimaIt.Dictionary.Application.Features.Configuration
 {
     public interface IConfigurationRepository
     {
+        Task<IEnumerable<ParameterEntity>> GetParametersAsync(ConfigurationParameterFilter filter, CancellationToken cancellationToken = default);
+        Task<List<ParameterValueEntity>> GetParametersValuesAsync(ParameterFilter filter, CancellationToken cancellationToken = default);
+        Task<int> InsertParameterValue(ParameterValueEntity parameterValueEntity, CancellationToken cancellationToken = default);
+        Task<int> UpdateParameterValue(ParameterValueEntity parameterValueEntity, CancellationToken cancellationToken = default);
+        Task<int> DeleteParameterValue(ParameterValueEntity parameterValueEntity, CancellationToken cancellationToken = default);
+        Task<Dictionary<string, Dictionary<string, string>>> GetAllParameters(int moduleId, CancellationToken cancellationToken = default);
+
+
     }
 }
