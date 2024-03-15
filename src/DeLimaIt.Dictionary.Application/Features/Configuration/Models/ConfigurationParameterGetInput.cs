e58c586302a446186d751c74cf22d60946f9d74e
Denis Lima <128059827+DenisMasterclass@users.noreply.github.com>
2024-03-14 22:59:41 -0300

    Models

--- src/DeLimaIt.Dictionary.Application/Features/Configuration/Models/ConfigurationParameterGetInput.cs
+++ src/DeLimaIt.Dictionary.Application/Features/Configuration/Models/ConfigurationParameterGetInput.cs
@@ -1,9 +1,4 @@
 ﻿using DelimaIt.Core.UseCases;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DeLimaIt.Dictionary.Application.Features.Configuration.Models
 {
--- src/DeLimaIt.Dictionary.Application/Features/Configuration/Models/ConfigurationParameterInput.cs
+++ src/DeLimaIt.Dictionary.Application/Features/Configuration/Models/ConfigurationParameterInput.cs
@@ -1,4 +1,6 @@
-﻿namespace DeLimaIt.Dictionary.Application.Features.Configuration.Models
+﻿using DelimaIt.Core.UseCases;
+
+namespace DeLimaIt.Dictionary.Application.Features.Configuration.Models
 {
     public enum OperationType: int
     {
@@ -6,7 +8,7 @@
         Update,
         Delete
     }
-    public sealed class ConfigurationParameterInput
+    public sealed class ConfigurationParameterInput : IRequest<ConfigurationParameterOutput>
     {
         public ConfigurationParameterInput(OperationType operation, int parameterId, string key, string value)
         {
@@ -16,9 +18,17 @@
             Value = value;
         }
 
+        public ConfigurationParameterInput(OperationType operation, int parameterId, string key, string value, Guid correlationId, Guid transactionId) : this(operation, parameterId, key, value)
+        {
+            CorrelationId = correlationId;
+            TransactionId = transactionId;
+        }
+
         public OperationType Operation { get; set; }
         public int ParameterId { get; set; }
         public string Key { get; set; }
         public string Value { get; set; }
+        public Guid CorrelationId { get; set; }
+        public Guid TransactionId { get; set; }
     }
 }
